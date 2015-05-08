599ba5442c52e9987c42a7df6613ec990e05c77c
AAAyag <491237847@qq.com>
2015-05-08 17:36:48 +0800

    delete string

--- C++Learning.cpp
+++ C++Learning.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	char*  cName ="hello, eric";  //
-	string sName = "eric zhang";
+
 	char cName2[8] = {'h', 'a', 'p', 'p','y'};
 	std::cout << "cName2:" << cName2 << endl;
 	int badNums[5] = {4, 12, 1, 5, 10};
@@ -14,7 +14,7 @@ int main()
 	std::cout << "badNums:"<< badNums<< endl;
 	std::cout << "cName:" << cName <<endl;
 	std::cout << "iName:" << iName;
-	std::cout << "sName:" << sName;
+
 	cin.get();
 	return 0;
 
