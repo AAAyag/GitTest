cafae32bed2116757f72e2c2cb9f22243bd3340e
AAAyag <491237847@qq.com>
2015-05-08 15:28:12 +0800

    second commit, pay attention to the char* and int*, char []and  int []

--- C++Learning.cpp
+++ C++Learning.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 int main()
 {
-	char*  cName ="hello, eric";
+	char*  cName ="hello, eric";  // char* ×Ö·û´® ÓëÆäËü
 	string sName = "eric zhang";
+	char cName2[6] = {'h', 'a', 'p', 'p','y'};
+	std::cout << "cName2:" << cName2 << endl;
+
 	int badNums[5] = {4, 12, 1, 5, 10};
 
 	int* iName = badNums;
