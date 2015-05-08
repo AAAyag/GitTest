b68539a04a19858bc97c9c4f5a676065deccf921
AAAyag <491237847@qq.com>
2015-05-08 16:21:39 +0800

    third commit

--- C++Learning.cpp
+++ C++Learning.cpp
@@ -4,11 +4,10 @@
 using namespace std;
 int main()
 {
-	char*  cName ="hello, eric";  // char* ×Ö·û´® ÓëÆäËü
+	char*  cName ="hello, eric";  //
 	string sName = "eric zhang";
-	char cName2[6] = {'h', 'a', 'p', 'p','y'};
+	char cName2[8] = {'h', 'a', 'p', 'p','y'};
 	std::cout << "cName2:" << cName2 << endl;
-
 	int badNums[5] = {4, 12, 1, 5, 10};
 
 	int* iName = badNums;
